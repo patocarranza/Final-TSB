0b06d2c8ca37662877fbc31351dc6a522d1175ba
Patricio Carranza <patocarranza@gmail.com>
2019-02-03 23:50:56 -0300

    tenia feo nombre
    Former-commit-id: 6562f1cbd0ba13b1b6754f8af321ed5c1feb7e30

--- HashTable/test/edu/utn/frc/tsb/HashtableTests.java
+++ HashTable/test/edu/utn/frc/tsb/HashtableTests.java
@@ -22,9 +22,9 @@ import static org.junit.Assert.*;
  *
  * @author Patricio
  */
-public class HashtableTestByPatricio {
+public class HashtableTests {
     
-    public HashtableTestByPatricio() {
+    public HashtableTests() {
     }
     
     @BeforeClass
